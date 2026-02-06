39248b7ec4adbe0dd91ba2c20a87fac34f080cd2
vanivadagiri <vani04.v@gmail.com>
2026-02-06 12:21:40 +0530

    Update first_program.py

--- first_program.py
+++ first_program.py
@@ -7,7 +7,7 @@ def view_student_details():
     for i,j in students.items():
         print(i,j["name"],j["marks"])
 
-add_student(1,"vani",98)
+add_student(1,"vanivaddagiri",98)
 add_student(2,"vinutha",98)
 add_student(3,"ajiy",100)
 add_student(4,"santosh", 100)
